1529532a505056e61c75f5f2554e69b96a6c559c
unknown <marcoaotrpaiva@gmail.com>
2025-04-17 17:03:17 +0100

    changed typeface import settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@
 import { Geist, Geist_Mono, Montserrat } from 'next/font/google';
 import './globals.css';
 import type { Metadata } from 'next';
-const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
-const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
+
 
 const montserrat = Montserrat({
   subsets: ['latin'],
